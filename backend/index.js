a37ec1ee9e3b1b26621cc5750231d74c5b356d7f
Chris-wav <HristosKalatzis@outlook.com.gr>
2025-09-05 19:05:14 +0300

    Backend changes

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const toDoRoutes = require("./routes/tasks.routes");
 
 // Enable CORS for specific front-end origins
